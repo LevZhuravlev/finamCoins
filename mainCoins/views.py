63c1267e96a54bca48325549e9b3b30529ec9657
LevZhuravlev <leoalexShuravl@gmail.com>
2021-03-05 14:53:00 +0300

    Auth RegAndIn Out

--- mainCoins/views.py
+++ mainCoins/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from .form import AuthUserForm, RegisterUserForm
 from django.contrib.auth.models import User
-
+from django.views.generic import CreateView
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -18,14 +19,20 @@ class MyProjectLoginView(LoginView):
     def get_success_url(self):
         return self.success_url
 
-from django.views.generic import CreateView
-
 class RegisterUserView(CreateView):
     model = User
     template_name = 'mainCoins/registration.html'
     success_url ='/'
     success_msg = 'Пользователь создан'
     form_class = RegisterUserForm
+
+    def form_valid(self, form):
+        form_valid = super().form_valid(form)
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        aut_user = authenticate(username=username, password=password)
+        login(self.request, aut_user)
+        return form_valid
 
 class MyProjectLogout(LogoutView):
 	next_page = '/'
