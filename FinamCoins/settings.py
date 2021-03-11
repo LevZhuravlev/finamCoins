d9fa0ecceffc44688070c53cc5af9df4f5b4f673
LevZhuravlev <leoalexShuravl@gmail.com>
2021-03-11 13:51:13 +0300

    without Heroc and static troubles

--- FinamCoins/settings.py
+++ FinamCoins/settings.py
@@ -126,5 +126,5 @@ DATABASES['default'].update(db_from_env)
 
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static')]
+# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+# STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static')]
