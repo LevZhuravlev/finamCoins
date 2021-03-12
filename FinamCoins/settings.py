92c860126106763d37f81970e7457ececa982044
LevZhuravlev <leoalexShuravl@gmail.com>
2021-03-12 08:56:52 +0300

    All

--- FinamCoins/settings.py
+++ FinamCoins/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,12 +119,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # heroku config:set DISABLE_COLLECTSTATIC=1
 
-STATIC_URL = '/static/'
 
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = (os.path.join(os.path.dirname(__file__), '..', 'static'))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 import dj_database_url
